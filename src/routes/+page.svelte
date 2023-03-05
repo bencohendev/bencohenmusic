3d618ed31477c0f9826ca0c0a1e3809668503c5a
Ben Cohen-Chen <ben@bencohen.dev>
2023-03-05 11:11:30 -0500

    barebones links to external sources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Welcome to Ben Cohen Music</h1>
+<p>
+	I'll be slowly working on rebuilding this website, but in the meantime you can check out the
+	following links
+</p>
+<div>
+	<ul>
+		<li>
+			<a href="https://eschatologyrecords.bandcamp.com/">Eschatology Records</a>
+		</li>
+    <li>
+
+      <ul>
+        Danny Fisher-Locchead Large Ensemble
+        <li>
+          <a href="https://dannyfisherlochhead.bandcamp.com/album/tools-of-the-abstract"
+            >Tools of the Abstract</a
+          >
+        </li>
+        <li>
+          <a href="https://dannyfisherlochhead.bandcamp.com/album/on-ceremony">On Ceremony</a>
+        </li>
+      </ul>
+    </li>
+		<li>
+			<a href="https://music.apple.com/us/artist/jffs-music-for-children/1392772597"
+				>Johnny Fuckface's Music For Children</a
+			>
+		</li>
+	</ul>
+</div>
