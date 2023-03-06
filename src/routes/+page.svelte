3b3c6d96588149ca419dfdf90cc31bb235778c43
Ben Cohen-Chen <ben@bencohen.dev>
2023-03-06 08:42:43 -0500

    tailwind and a bit of styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,45 @@
-
-<h1>Welcome to Ben Cohen Music</h1>
-<p>
-	I'll be slowly working on rebuilding this website, but in the meantime you can check out the
-	following links
-</p>
 <div>
-	<ul>
-		<li>
-			<a href="https://eschatologyrecords.bandcamp.com/">Eschatology Records</a>
-		</li>
-    <li>
 
+  <h1 class="text-4xl flex justify-center my-8">Welcome to Ben Cohen Music</h1>
+</div>
+<div class="flex justify-center h-screen">
+  <div>
+    <p class="mb-8">
+      I'll be slowly working on rebuilding this website, but in the meantime you can check out the
+      following links
+    </p>
+    <div>
       <ul>
-        Danny Fisher-Locchead Large Ensemble
-        <li>
-          <a href="https://dannyfisherlochhead.bandcamp.com/album/tools-of-the-abstract"
-            >Tools of the Abstract</a
+        <li class="my-2">
+          <div class="flex"><a class="text-blue-600 mr-1" href="https://eschatologyrecords.bandcamp.com/">Eschatology Records</a>
+            <p>is my small label where I release most of my music as well as the music of a few friends</p>          
+          </div>
+        </li>
+        <li>    
+          <li class="my-2">
+            <h3 class="text-lg">Danny Fisher-Locchead Large Ensemble</h3>
+            <div class="flex mr-6">
+              <div class="my-2 mx-4">
+                <a class="text-blue-600" href="https://dannyfisherlochhead.bandcamp.com/album/tools-of-the-abstract"
+                  >Tools of the Abstract</a
+                >
+              </div>
+              <div class="my-2 mx-4">
+                <a class="text-blue-600" href="https://dannyfisherlochhead.bandcamp.com/album/on-ceremony">On Ceremony</a>
+              </div>
+            </div>
+          </li>
+
+        <li class="my-2">
+          <a class="text-blue-600" href="https://music.apple.com/us/artist/jffs-music-for-children/1392772597"
+            >Johnny Fuckface's Music For Children</a
           >
         </li>
-        <li>
-          <a href="https://dannyfisherlochhead.bandcamp.com/album/on-ceremony">On Ceremony</a>
-        </li>
       </ul>
-    </li>
-		<li>
-			<a href="https://music.apple.com/us/artist/jffs-music-for-children/1392772597"
-				>Johnny Fuckface's Music For Children</a
-			>
-		</li>
-	</ul>
-</div>
-<div>
-  <h3>Contact</h3>
-  <div><a href="ben@bencohenmusic.com">ben @ bencohenmusic.com</a></div>
+    </div>
+    <div>
+      <h3 class="text-xl mt-4">Contact</h3>      
+      <div><a class="text-blue-600" href="ben@bencohenmusic.com">ben @ bencohenmusic.com</a></div>
+    </div>
+  </div>
 </div>
